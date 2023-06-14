5d267314b8f3a3063a83cfdcec5dbd7909e85145
Thom Treebus <thomtreebus@gmail.com>
2023-06-14 13:25:08 +0100

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Thom Treebus on 14/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
